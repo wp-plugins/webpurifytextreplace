bc775efed4d0f624e6e1b3d149b4d3190d50cf69
webpurify <webpurify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-30 21:09:03 +0000

    Updated for 2.8.x
    git-svn-id: https://plugins.svn.wordpress.org/webpurifytextreplace/trunk@141069 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- WebPurifyTextReplace-options.php
+++ WebPurifyTextReplace-options.php
@@ -1,31 +1,92 @@
 <?php
 /*
-please see http://www.webpurify.com/wp-plugin.php for more information.
+Plugin Name: WebPurifyTextReplace
+Plugin URI: http://www.webpurify.com/wp-plugin.php
+Description: Filters user comments for profanity BEFORE they are inserted into the database using the WebPurify API. All Legacy comments (ie all comments posted before the time you starting using this API&nbsp;will not be checked)  Replaces each letter of profane words with a "*". Please register for a key at: <a target="new" href="http://www.webpurify.com/apply.php">http://www.webpurify.com/apply.php</a>
+Version: 1.5
+Author: WebPurify
+Author URI: http://www.webpurify.com
+*/
+/*  Copyright 2009  WebPurify  (email : comments@webpurify.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-load_plugin_textdomain('WebPurifyTextReplace');
 
-// set paramters to default, if not exists
+function webpurify_options_page() {
+	add_options_page('WebPurify Options', 'WebPurify', 9, 'webpurify/WebPurifyTextReplace.php');
+}
 
-add_option('webpurify_userkey', '');
+function WebPurifyTextReplace($commentID) {
+    global $wpdb;
 
-if(isset($_POST[webpurify_key]))
-    {
-      update_option('webpurify_userkey',$_POST[webpurify_key]);
+    $API_KEY = get_option('webpurify_userkey');
+
+    $table_name = $wpdb->prefix . "comments";
+    $getcomment = "SELECT comment_content from ".$table_name." where comment_ID = ".$commentID.";";
+    $content = $wpdb->get_var($getcomment);
+
+
+    $params = array(
+      'api_key' => $API_KEY,
+      'method' => 'webpurify.live.replace',
+      'text' => $content,
+      'replacesymbol' => '*'
+    );
+
+
+    $encoded_params = array();
+
+    foreach ($params as $k => $v){
+        $encoded_params[] = urlencode($k).'='.urlencode($v);
     }
 
-$userkey = get_option('webpurify_userkey');
+#
+# call the API and decode the response
+#
+    $url = "http://www.webpurify.com/services/rest/?".implode('&', $encoded_params);
 
+    $rsp = file_get_contents($url);
+    $ar = ParseXML($rsp);
+
+    $update_comment = "UPDATE ".$table_name." set comment_content = '".mysql_escape_string($ar)."' where comment_ID = ".$commentID.";";
+    $results = $wpdb->query($update_comment);
+
+}
+
+function ParseXML($xml) {
+// Gets XML in a string and parses it into an array.
+
+// Create the parser object
+$parser = xml_parser_create();
+
+xml_parse_into_struct($parser, trim($xml), &$structure, &$index);
+xml_parser_free($parser);
+
+// Hack up the XML and put it into the array
+foreach($structure as $s)
+{
+      if ($s["tag"] == "TEXT") {
+           $cleancontent = $s['value'];
+      }
+}
+
+return $cleancontent;
+}
+
+add_action('admin_menu', 'webpurify_options_page');
+add_action('comment_post','WebPurifyTextReplace');
+//add_filter('comment_text','WebPurifyTextReplace');
 ?>
-<div class="wrap"> 
-	<h2><?php _e('Configure: WebPurify Plugin', 'WebPurifyTextReplace') ?></h2>
-	<p><?php _e('In order to use this plugin you must enter your "WebPurify API Key."<br/><br/><br/>Apply for a FREE WebPurify API at: <a href="http://www.webpurify.com/apply.php"  target=_blank >www.webpurify.com/apply.php</a>', 'WebPurifyTextReplace') ?></strong></p>
-	<form name="form1" method="post" action="<?php echo(get_option('siteurl') . '/wp-admin/admin.php?page=webpurify/WebPurifyTextReplace-options.php'); ?>">
-
-		<fieldset class="options">
-			    <?php _e('Enter WebPurify API Key', 'WebPurifyTextReplace') ?>: <input type="text" name="webpurify_key" value="<?= $userkey ?>" />
-                <br/><br />
-                <p class="submit"><input type="submit" name="store_key" value="<?php _e('Save API Key &amp; Activate Plugin', 'WebPurifyTextReplace') ?>" ></p>
-		</fieldset>
-	</form> 
-</div>
--- readme_webpurify.txt
+++ readme_webpurify.txt
@@ -1,7 +1,7 @@
 === WebPurify for WordPress ===
 Authors: WebPurify
 
-Contact: comments@webpurify.com
+Contact: support@webpurify.com
 
 == Overview ==
 The WebPurify Plugin allows your to use the powerful WebPurify Profanity Filter API to stop prfanity from making onto your wordpress blog.
@@ -16,12 +16,12 @@ http://www.webpurify.com/wp-plugin.php
 == Installation ==
 1. Unzip the file and then upload the entire "webpurify" folder to wp-content/plugins directory. Make sure to add the whole folder, not the individual files.
 2. Activate it in the Plugin options.
-3. Go to the Admin -> Options -> WebPurify page to enter your FREE WebPurify API Key. 
+3. Go to the Admin -> Options -> WebPurify page to enter your WebPurify API Key. 
 
-You can get an API key by goign to: http://www.webpurify.com/apply.php
+You can get an API key by goign to: http://www.webpurify.com/
 
 == Support ==
-Have any problems, questions, or ideas? Please contact us at comments@webpurify.com
+Have any problems, questions, or ideas? Please contact us at support@webpurify.com
 
 
 
